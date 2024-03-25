cc7a60f36ee1ada346497c666d98129c6225065c
Stefano Babini <stefano.babini5@studio.unibo.it>
2024-03-25 11:57:49 +0100

    ex 6

--- src/main/scala/tasks/monads/Ex6TryModel.scala
+++ src/main/scala/tasks/monads/Ex6TryModel.scala
@@ -3,16 +3,12 @@ package tasks.monads
 import u04.monads.Monads.Monad
 import u04.monads.Monads.Monad
 
-/**
-  * Exercise 6: 
-    This module contains the implementation of a Try monad, which is a monad that 
-    represents a computation that may fail. 
-    Try to follow these steps:
-    - Look at the implementation of Try, that is similar to the one of Optional
-    - Try go define the Monad instance for Try
-      - flatMap should consider only the Success case
-      - in case of Failure, it should return the exception (fail fast)
-    - Verify that the main works as expected
+/** Exercise 6: This module contains the implementation of a Try monad, which is
+  * a monad that represents a computation that may fail. Try to follow these
+  * steps: \- Look at the implementation of Try, that is similar to the one of
+  * Optional \- Try go define the Monad instance for Try \- flatMap should
+  * consider only the Success case \- in case of Failure, it should return the
+  * exception (fail fast) \- Verify that the main works as expected
   */
 object Ex6TryModel:
   private enum TryImpl[A]:
@@ -23,30 +19,34 @@ object Ex6TryModel:
 
   def success[A](value: A): Try[A] = TryImpl.Success(value)
   def failure[A](exception: Throwable): Try[A] = TryImpl.Failure(exception)
-  def exec[A](expression: => A): Try[A] = try success(expression) catch failure(_)
+  def exec[A](expression: => A): Try[A] = try success(expression)
+  catch failure(_)
 
-  extension [A](m: Try[A]) 
+  extension [A](m: Try[A])
     def getOrElse[B >: A](other: B): B = m match
       case TryImpl.Success(value) => value
-      case TryImpl.Failure(_) => other
+      case TryImpl.Failure(_)     => other
 
   given Monad[Try] with
-    override def unit[A](value: A): Try[A] = ???
-    extension [A](m: Try[A]) 
+    override def unit[A](value: A): Try[A] = success(value)
 
-      override def flatMap[B](f: A => Try[B]): Try[B] = ??? 
-      
-@main def main: Unit = 
+    extension [A](m: Try[A])
+      override def flatMap[B](f: A => Try[B]): Try[B] = m match
+        case TryImpl.Success(a) => f(a)
+        case TryImpl.Failure(exception) => failure(exception)
+
+@main def main: Unit =
   import Ex6TryModel.*
 
-  val result = for 
+  val result = for
     a <- success(10)
     b <- success(30)
   yield a + b
 
   assert(result.getOrElse(-1) == 40)
+  println(result.getOrElse(-1))
 
-  val result2 = for 
+  val result2 = for
     a <- success(10)
     b <- failure(new RuntimeException("error"))
     c <- success(30)
