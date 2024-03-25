ee2e53c5665750f2423b9c4f5146d8fe55ac20da
Stefano Babini <stefano.babini5@studio.unibo.it>
2024-03-25 11:42:08 +0100

    ex 4

--- src/main/scala/tasks/typeclasses/Ex4Summables.scala
+++ src/main/scala/tasks/typeclasses/Ex4Summables.scala
@@ -1,8 +1,8 @@
 package u04lab
-import u03.Sequences.* 
+import u03.Sequences.*
 import Sequence.*
 
-/*  Exercise 4: 
+/*  Exercise 4:
  *  - Complete the implementation of ad-hoc polymorphic sumAll, using summable.sum and summable.zero
  *  - Write givens also for Summable[Double], Summable[String]
  *  - Uncomment in the main and check if everything works
@@ -12,39 +12,44 @@ object Ex4Summables:
 
   def sumAllInt(seq: Sequence[Int]): Int = seq match
     case Cons(h, t) => h + sumAllInt(t)
-    case _ => 0
+    case _          => 0
 
   trait Summable[A]:
     def sum(a1: A, a2: A): A
     def zero: A
 
-  def sumAll[A: Summable](seq: Sequence[A]) = 
+  def sumAll[A: Summable](seq: Sequence[A]): A =
     val summable = summon[Summable[A]]
-    ???  // complete here
+    seq match
+      case Cons(h, t) => summable.sum(h, sumAll(t))
+      case Nil()      => summable.zero
 
   given Summable[Int] with
     def sum(a1: Int, a2: Int): Int = a1 + a2
     def zero: Int = 0
-  
+
+  given Summable[Double] with
+    def sum(a1: Double, a2: Double): Double = a1 + a2
+    def zero: Double = 0.0
+
+  given Summable[String] with
+    def sum(a1: String, a2: String): String = a1 + a2
+    def zero: String = ""
+
   // write givens for Summable[Double] and Summable[String]
 
   @main def trySummables =
-    val si = Cons(10, Cons(20, Cons(30, Nil())))  
+    val si = Cons(10, Cons(20, Cons(30, Nil())))
     println:
       sumAllInt(si) // 60
-
-    /* uncomment from here   
 
     println:
       sumAll(si) // 60
 
-    val sd = Cons(10.0, Cons(20.0, Cons(30.0, Nil())))  
+    val sd = Cons(10.0, Cons(20.0, Cons(30.0, Nil())))
     println:
       sumAll(sd) // 60.0
 
-    val ss = Cons("10", Cons("20", Cons("30", Nil())))  
+    val ss = Cons("10", Cons("20", Cons("30", Nil())))
     println:
       sumAll(ss) // "102030"
-
-    */  
-
