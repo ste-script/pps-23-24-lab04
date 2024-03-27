cd5fce4ac06fc6d9202e3d0ac73c9269e9cf5de2
Stefano Babini <stefano.babini5@studio.unibo.it>
2024-03-27 16:40:15 +0100

    ex 4

--- src/main/scala/tasks/adts/Ex3Stack.scala
+++ src/main/scala/tasks/adts/Ex3Stack.scala
@@ -3,9 +3,9 @@ package tasks.adts
 import u03.Sequences.*
 import u03.Optionals.*
 
-/*  Exercise 3: 
+/*  Exercise 3:
  *  Implement a Stack ADT
- *  Suggestion: 
+ *  Suggestion:
  *  - push adds an element and returns the new stack
  *  - pop returns:
  *  -- empty optional is stack is empty
@@ -20,11 +20,16 @@ object Ex3Stacks:
       def push(a: A): Stack[A]
       def pop(a: A): Optional[(A, Stack[A])]
       def asSequence(): Sequence[A]
-  
+
   object StackImpl extends StackADT:
-    type Stack[A] = Nothing
-    def empty[A]: Stack[A] = ???
+    case class Stack[A](sequence: Sequence[A])
+    def empty[A]: Stack[A] = Stack(Sequence.Nil())
     extension [A](stack: Stack[A])
-      def push(a: A): Stack[A] = ???
-      def pop(a: A): Optional[(A, Stack[A])] = ???
-      def asSequence(): Sequence[A] = ???
+      def push(a: A): Stack[A] =
+        Stack(Sequence.Cons(a, stack.sequence))
+      def pop(a: A): Optional[(A, Stack[A])] =
+        stack.sequence match {
+          case Sequence.Cons(h, t) if h == a => Optional.Just((h, Stack(t)))
+          case _                             => Optional.Empty()
+        }
+      def asSequence(): Sequence[A] = stack.sequence
